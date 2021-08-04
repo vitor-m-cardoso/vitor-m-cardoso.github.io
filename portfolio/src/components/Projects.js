d06e0033cc801e47c1759ab99689b7c82fc3269b
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-04 13:32:56 -0300

    adiciona projeto pixel art a aba de projetos

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import pixelArtProject from './img/projectsImg/pixelArtProject.png'
 import './css/Components.css';
 
 class Home extends Component {
   render() {
     return (
-      <div className="d-flex ajust-margin home-component justify-content-center align-items-center text-white">
-        <h1>Em construção...</h1>
+      <div className="ajust-margin home-component text-white">
+        <Card
+          style={{ width: '14rem', backgroundColor: 'rgb(28, 31, 46)' }}
+          className="text-center align-items-center"
+        >
+          <Card.Img variant="top" src={ pixelArtProject } />
+          <Card.Title className="my-2">Projeto Pixel Art</Card.Title>
+          <Card.Text>
+            Pagina criada utilizando HTML, CSS e JavaScript.
+          </Card.Text>
+          <Button
+            variant="outline-secondary btn-sm"
+            className="mb-2"
+            style={ { maxWidth: '160px' } }
+          >
+            <a
+              href="https://github.com/vitor-m-cardoso/project-pixel-art"
+              target="_blank"
+              rel="noreferrer"
+              style={ { textDecoration:"none", color:'white' } }
+            >
+              Repositorio no GitHub
+            </a>
+          </Button>
+          <Button
+            variant="outline-secondary btn-sm"
+            style={ { maxWidth: '160px' } }
+          >
+            <a
+              href="https://vitor-m-cardoso.github.io/project-pixel-art/"
+              target="_blank"
+              rel="noreferrer"
+              style={ { textDecoration:"none", color:'white' } }
+            >
+              Pagina do Projeto
+            </a>
+          </Button>
+        </Card>
       </div>
     )
   }
