792b660a8c5c62b984c686615f028cc89f775f65
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-08-12 13:05:12 -0300

    adiciona pasta data e adiciona projeto caravan

---
+++ portfolio/src/components/data/projectData.js
@@ -0,0 +1,21 @@
+import pixelArtProject from '../img/projectsImg/pixelArtProject.png'
+import caravanProject from '../img/projectsImg/caravanProject.png'
+
+const data = [
+  {
+    image: pixelArtProject,
+    title: "Projeto Pixel Art",
+    text: "Pagina criada utilizando HTML, CSS e JavaScript.",
+    projectLink: "https://vitor-m-cardoso.github.io/project-pixel-art/",
+    gitHubLink: "https://github.com/vitor-m-cardoso/project-pixel-art",
+  },
+  {
+    image: caravanProject,
+    title: "Projeto Caravan",
+    text: "Pagina criada utilizando HTML, CSS e Bootstrap.",
+    projectLink: "https://vitor-m-cardoso.github.io/project-caravan-bootstrap/index.html",
+    gitHubLink: "https://github.com/vitor-m-cardoso/project-caravan-bootstrap",
+  }
+];
+
+export default data;
